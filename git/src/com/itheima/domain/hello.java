d6f681ef714cc1194769701b2ea0ebf25149c970
admit <admit@itheima.com>
2018-12-17 10:18:18 +0800

    提交数据

---
+++ git/src/com/itheima/domain/hello.java
@@ -0,0 +1,10 @@
+package com.itheima.domain;
+    public class hello {
+    public static void main(String[] args) {
+        System.out.println("111111111111");
+        System.out.println("111111111111");
+        System.out.println("111111111111");
+        System.out.println("111111111111");
+        System.out.println("111111111111");
+    }
+}
